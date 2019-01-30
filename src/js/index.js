401b20f4fc36c61711923456bf91b6fd41a19fb4
Fernando Guevara <fernando@alertandote.com>
2019-01-30 13:29:19 -0600

    (Add):Typed principal [scripts.js _hero.scss index.pug]

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 import sass from '../scss/main.scss';
+import scripts  from './scripts';
 // import { module } from '../module';
+
---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+// Typed text
+const typed = new Typed('.typed', {
+    stringsElement: '.typed__strings',
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 60
+});
